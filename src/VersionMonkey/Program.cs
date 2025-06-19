614fe3f07b52d8197526eb2ff56c874a6042daea
David Boike <david.boike@gmail.com>
2025-06-19 17:11:14 -0500

    Program to dump out env vars

--- src/VersionMonkey/Program.cs
+++ src/VersionMonkey/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Collections;
+
+Console.WriteLine("VersionMonkey");
+
+Console.WriteLine("Environment variables:");
+foreach (DictionaryEntry pair in Environment.GetEnvironmentVariables())
+{
+    Console.WriteLine($"  - {pair.Key}={pair.Value}");
+}
